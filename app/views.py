77c7906a562151f22b77eebb1969648856516d40
12321231 <52814667+12321231@users.noreply.github.com>
2021-12-17 22:00:11 +0800

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -7,3 +7,27 @@ class Index(View):
     def get(self,request):
         return render(request,'index.html')
 
+class aboutus(View):
+    def get(self,request):
+        return render(request,'about-us.html')
+
+class Error(View):
+    def get(self,request):
+        return render(request,'404.html')
+
+class Explain(View):
+    def get(self,request):
+        return render(request,'说明.html')
+
+#class Contribution(View):
+#    def get(self,request):
+#        contribute_data = []
+#        conact = {}
+#        if request.POST:
+#            conact = request.POST.get('urn')
+#            print(conact)
+#            print("1")
+#            contribute_data = {'value': 0, 'name': '0'}
+#        else:
+#            contribute_data = {'value': 0, 'name': ''}
+#        return render(request,'contribution.html', {"contribute_data": contribute_data} )
